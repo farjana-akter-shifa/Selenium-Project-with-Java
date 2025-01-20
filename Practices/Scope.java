e63c4c78f937525122d691e278b1ca5b8f467237
farjana-akter-shifa <shifafarjana634@gmail.com>
2025-01-20 19:43:47 +0100

    Automated code that can count Link on a page individullay.

---
+++ Practices/Scope.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scope {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		//1. How many links are available on a specific page/count of links
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		System.out.println(driver.findElements(By.tagName("a")).size());
+		
+		//2. If I want to count only footers link
+		
+		WebElement footerdriver = driver.findElement(By.id("gf-BIG"));
+		System.out.println(footerdriver.findElements(By.tagName("a")).size());
+		
+		//3. If i want to more specific like one column from footer
+		
+		WebElement columndriver= footerdriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+		System.out.println(columndriver.findElements(By.tagName("a")).size());
+		
+		//4. click on each link in the column and check if the pages are opening
+		for(int i=1; i<columndriver.findElements(By.tagName("a")).size();i++)
+		{
+			String clickonlinkTab = Keys.chord(Keys.CONTROL,Keys.ENTER);
+			columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
+			Thread.sleep(5000L);
+		}
+
+	}
+
+}
